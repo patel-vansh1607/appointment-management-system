2146a50bf7dc2f0da3ed025c145390224ff11e80
Vansh Patel <patel.vansh1607@gmail.com>
2025-03-25 09:19:49 +0300

    add handleSubmit function to AppointmentForm for form submission

--- src/components/AppointmentForm.js
+++ src/components/AppointmentForm.js
@@ -4,6 +4,13 @@ const AppointmentForm = ({ addAppointment }) => {
 	const [name, setName] = useState("");
 	const [date, setDate] = useState("");
 
+    const handleSubmit = (e) => {
+        e.preventDeafault();
+        addAppointment({name, date});
+        setName("");
+        setDate("");
+    }
+
     return(
         <div>
             <h1> hi</h1>
