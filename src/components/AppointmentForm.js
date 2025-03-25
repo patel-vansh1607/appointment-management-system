e95e42be88d388ef16f40e2f78871c6403bcd451
Vansh Patel <patel.vansh1607@gmail.com>
2025-03-25 09:18:49 +0300

    add AppointmentForm component with initial state setup

---
+++ src/components/AppointmentForm.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+const AppointmentForm = ({ addAppointment }) => {
+	const [name, setName] = useState("");
+	const [date, setDate] = useState("");
+
+    return(
+        <div>
+            <h1> hi</h1>
+        </div>
+    )
+}
