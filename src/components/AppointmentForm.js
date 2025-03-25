32422d5d46272434c7d45935568fc64c77a51fa6
Vansh Patel <patel.vansh1607@gmail.com>
2025-03-25 09:25:09 +0300

    fix: correct handleSubmit function and improve form structure in AppointmentForm

--- src/components/AppointmentForm.js
+++ src/components/AppointmentForm.js
@@ -1,33 +1,37 @@
+// AppointmentForm.js
+
 import React, { useState } from "react";
 
 const AppointmentForm = ({ addAppointment }) => {
 	const [name, setName] = useState("");
 	const [date, setDate] = useState("");
 
-    const handleSubmit = (e) => {
-        e.preventDeafault();
-        addAppointment({name, date});
-        setName("");
-        setDate("");
-    }
-    return(
-        <div className="container">
-           <form onSubmit={handleSubmit}>
-                <div className="row">
-                    <div className="col-25">
-                        <label for="fname">Full Name</label>
-                    </div>
-                    <div className="col-75">
-                    <input
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		addAppointment({ name, date });
+		setName("");
+		setDate("");
+	};
+
+	return (
+		<div class="container">
+			<form onSubmit={handleSubmit}>
+				<div class="row">
+					<div class="col-25">
+						<label for="fname">Full Name</label>
+					</div>
+					<div class="col-75">
+						<input
 							type="text"
 							id="fname"
 							name="firstname"
 							placeholder="Your name.."
 							value={name}
-							onChange={(e) => setName(e.target.value)}/>
-                    </div>
-                </div>
-                <div class="row">
+							onChange={(e) => setName(e.target.value)}
+						/>
+					</div>
+				</div>
+				<div class="row">
 					<div class="col-25">
 						<label for="fname">Appointment Date: </label>
 					</div>
@@ -41,7 +45,13 @@ const AppointmentForm = ({ addAppointment }) => {
 							onChange={(e) => setDate(e.target.value)}
 						/>
 					</div>
-           </form>
-        </div>
-    )
-}
+				</div>
+				<div class="row">
+					<input type="submit" value="Add Appointment" />
+				</div>
+			</form>
+		</div>
+	);
+};
+
+export default AppointmentForm;
